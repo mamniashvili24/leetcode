e901d2b76f1e5139abbe114971ff0bce4ee2f67b
Nika Mamniashvili <mamniashvili24@gmail.com>
2024-09-09 01:07:05 +0400

    change duplicate integer implementation

--- Duplicate Integer.py
+++ Duplicate Integer.py
@@ -3,8 +3,11 @@ from typing import List
 
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        for i in range(len(nums)):
-         for j in range(i+1, len(nums)):
-             if nums[i] == nums[j]:
-                 return True
+        for num in nums:
+            for num1 in nums:
+                if  num == num1:
+                    return True
         return False
+
+
+print(Solution().hasDuplicate([1,2,3,4]))
