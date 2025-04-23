aec01416c85fa5f3c00e55ee1392d01bf5401d77
Nika Mamniashvili <mamniashvili24@gmail.com>
2025-04-23 18:12:19 +0400

    implement linked list and stack

---
+++ linked_list.py
@@ -0,0 +1,46 @@
+class node:
+    def __init__(self, value) -> None:
+        self.value = value
+        self.next = None
+
+class linked_list:
+    def __init__(self) -> None:
+        self.element = None
+
+    def insert_first(self, value):
+        new_node = node(value)
+        new_node.next = self.element
+        self.element = new_node
+
+    def insert_last(self, value):
+        if not self.element:
+            self.element = node(value)
+            return
+        
+        current = self.element
+        while current.next:
+            current = current.next
+
+        current.next = node(value)
+
+    def remove_first(self):
+        if not self.element:
+            print('linked list is empty')
+            return
+        
+        self.element = self.element.next
+
+    def remove_last(self):
+        if not self.element:
+            print('linked list is empty')
+            return
+        
+        if not self.element.next:
+            self.element = None
+            return
+
+        current = self.element
+        while current.next and current.next.next:
+            current = current.next
+
+        current.next = None
